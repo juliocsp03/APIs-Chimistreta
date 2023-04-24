8f3537c033cc753194ffd244aead9f483a4a3093
Julio Salazar <juliocsp03@gmail.com>
2023-04-23 18:50:58 -0600

    Consulta con where like xd

--- main.py
+++ main.py
@@ -24,18 +24,6 @@ def getLevels():
 		res = cursor.fetchall()
 		cursor.execute(f"SELECT max(level) FROM levels WHERE methodology_id = {methodology_id};")
 		total_levels = cursor.fetchone()
-		# more_levels = json.dumps(more_levels)[0]
-		# more_levels = more_levels[1
-		# print("hay mas niveles: ", more_levels['max'])
-		# levels = []
-		# for item in res:
-		# 	level_object = {
-		# 		"id": item[0],
-		# 		"name_level": item[1],
-		# 		"level": item[2],
-		# 		"methodology_id": item[3]
-		# 	}
-		# 	levels.append(level_object)
 		more_levels = False
 		if total_levels['max'] > level:
 			more_levels = True
@@ -51,19 +39,28 @@ def getProducts():
 	data = request.json
 	levels = data.get("levels")
 	methodology_id = data.get('methodology_id')
+	strict = data.get('strict')
 	levels = json.dumps(levels)
-	with conn.cursor(cursor_factory=RealDictCursor) as cursor:
-		cursor.execute(f"""SELECT id, methodology_id, path, levels, extension FROM methodology_instance WHERE levels->>'levels' = '{levels}' AND methodology_id = {methodology_id};""")
-		res = cursor.fetchall()
-		response = {
-			"quantity": len(res),
-			"products": res
-		}
-		return response
-	# print("niveles: ", json.dumps(levels))
-	# print(f"""SELECT id, methodology_id, path, levels, extension FROM methodology_instance WHERE levels->>'levels' = '{levels}' AND methodology_id = {methodology_id};""")
-	# return data	
-
+	if strict == True:
+		with conn.cursor(cursor_factory=RealDictCursor) as cursor:
+			cursor.execute(f"""SELECT id, methodology_id, url, levels, extension FROM methodology_instance WHERE levels->>'levels' = '{levels}' AND methodology_id = {methodology_id};""")
+			res = cursor.fetchall()
+			response = {
+				"quantity": len(res),
+				"products": res
+			}
+			return response
+	elif strict == False:
+		levels = levels.replace('[', "%")
+		levels = levels.replace(']', '%')
+		with conn.cursor(cursor_factory=RealDictCursor) as cursor:
+			cursor.execute(f"""SELECT id, methodology_id, url, levels, extension FROM methodology_instance WHERE levels->>'levels' LIKE '{levels}' AND methodology_id = {methodology_id};""")
+			res = cursor.fetchall()
+			response = {
+				"quantity": len(res),
+				"products": res
+			}
+			return response
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
